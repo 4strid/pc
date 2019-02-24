d3e6ac121e50b21a7e018bfde6bbaa9d1d582ef3
Peter Fesz-Nguyen <pfesznguyen@simpleviewinc.com>
2019-02-23 22:39:19 -0700

    2019-02-23 lots of random crap in vimrc, a shoddy copy of bashrc, and some fun bin scripts

--- bin/chrome2.bat
+++ bin/chrome2.bat
@@ -1,1 +1,1 @@
-start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --user-data-dir=C:\Users\pfesznguyen\chrome_profiles\chrome2
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --user-data-dir=C:\Users\pfesznguyen\chrome_profile_2
